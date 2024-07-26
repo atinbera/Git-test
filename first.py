764ec40b7f46a3ae83aedbaac03396a5e4b7b9cb
Atin Bera <atinb.cse.jisu21@gmail.com>
2024-07-26 08:47:07 +0530

    Third commit

--- first.py
+++ first.py
@@ -1,8 +1,11 @@
-print("Hi dear, how are you?")
+print("Hi dear, My name is Atin from kolkata.")
 
 for i in range(5):
     print("Sunny")
 
 a=5
 b=8
-print(a&b)
+print(a|b)
+
+sp=[1,2,3,4,5]
+print(len(sp))
